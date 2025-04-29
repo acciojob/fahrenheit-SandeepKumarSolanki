d06ffd20876b6c7562c172bd7ee86d832038aa7c
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-29 10:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	celsius = +celsius
 	if(celsius > -273.15°C ){
 		return (32 + celsius );
 	}	
 }
 
 // Do not change the code below
-const celsius = prompt("Enter Celsius:");
-alert(toFahrenheit(Number(celsius)));
+// const celsius = prompt("Enter Celsius:");
+// alert(toFahrenheit(Number(celsius)));
